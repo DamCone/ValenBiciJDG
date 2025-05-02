9b582d31e341777d7e40c87e6d30bfbd77f72a88
Damián Conejos <damiconejos@gmail.com>
2025-05-02 11:02:11 +0200

    Implementación web php y css

---
+++ ValenbiciApi/miweb/mapearbicis.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mapa de Estaciones Valenbisi</title>
+</head>
+<body>
+    <h1>Mapa de Estaciones (En construcción - Tarea 7)</h1>
+    <p><a href="index.php">Volver al listado</a></p>
+</body>
+</html>
